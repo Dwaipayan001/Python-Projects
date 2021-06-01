a03bc470ca6f0aba28676d2eac0ed2396cda92d0
DWAIPAYAN BANDYOPADHYAY <71640696+Dwaipayan001@users.noreply.github.com>
2021-06-01 18:06:02 +0530

    Update Talking Dictionary.py

--- Talking Dictionary.py
+++ Talking Dictionary.py
@@ -1,3 +1,8 @@
+"""Python Program to build a voice controlled Talking Dictionary using pyttsx3 (python text to speech) module , json (to parse the dictionary json file and use it in the program)
+speech_recognition module to continue the conversation with the program and difflib (get close matches) if someone enters a mispelled word then it will search for the closest correct
+one in the dictionary json file and say it out loud"""
+
+#importing the modules
 import pyttsx3
 import json
 from difflib import get_close_matches
@@ -7,7 +12,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-# talking portion
+# setting the rate of speech of the AI
 engine = pyttsx3.init()
 rate = engine.getProperty('rate')
 engine.setProperty('rate', 175)
@@ -17,6 +22,7 @@ engine.setProperty('volume', 1.0)
 
 voices = engine.getProperty('voices')
 
+#Greetings from the AI and the part to select our assistant (Male or Female)
 engine.say("""WELCOME TO THE TALKING DICTIONARY.
 listen to the instructions carefully :
 Do you want Male assistant or female assistant ?
@@ -24,23 +30,27 @@ Say Male for Male voice
 Say Female for Female voice""")
 engine.runAndWait()
 
+#Getting the first voice input from the user and using adjust_ambient method to remove the noise in the background.
 with mic as source:
     r.adjust_for_ambient_noise(source, duration=0.2)
     audio = r.listen(source)
 
+#Recognizing the voice input by user.    
 audio = r.recognize_google(audio)
-# voice_choice = int(input("Do you want Male or Female voice ?"))
 
 
-# voice_choice == 0 or
+
+# condition to check if the input is mail or female
 if audio == "Female".lower():
     engine.setProperty('voice', voices[1].id)
 else:
     engine.setProperty('voice', voices[0].id)
 
+#importing the dictionary json file
 data = json.load(open("E:\pythonProjects2\dictionary_compact.json"))
 
 
+#dictionary function where the main magic happens
 def dictionary(word):
     if word in data:
         print(data[word])
@@ -63,7 +73,7 @@ def dictionary(word):
     else:
         pass
 
-
+#some documentation 
 print("====================================================== \n")
 print("\t WELCOME TO THE TALKING DICTIONARY \n")
 print("====================================================== \n")
