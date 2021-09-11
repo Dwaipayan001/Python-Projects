bcd7edc02fb744f1039a7de87159b3d4d793e701
DWAIPAYAN BANDYOPADHYAY <71640696+Dwaipayan001@users.noreply.github.com>
2021-09-11 20:12:06 +0530

    Add files via upload

---
+++ Voice to Text/Voice to Text.py
@@ -0,0 +1,74 @@
+import pyttsx3
+import speech_recognition as sr
+import os
+
+
+
+file = open("test.txt","w")
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 175)
+
+volume = engine.getProperty('volume')
+engine.setProperty('volume', 1.0)
+
+voices = engine.getProperty('voices')
+
+engine.say("""WELCOME SIR !
+I AM HERE TO HELP YOU.
+PLEASE SAY MALE FOR MALE ASSISTANT
+PLEASE SAY FEMALE FOR FEMALE ASSISTANT""")
+engine.runAndWait()
+
+with mic as source:
+    r.adjust_for_ambient_noise(source, duration=0.2)
+    audio = r.listen(source)
+
+audio = r.recognize_google(audio)
+
+if audio == "Female".lower():
+    engine.setProperty('voice', voices[1].id)
+else:
+    engine.setProperty('voice', voices[0].id)
+
+while True:
+    engine.say("PLEASE TELL ME THE NAME OF THE SOFTWARE YOU WANT TO OPEN OR SAY EXIT TO STOP ME !")
+    engine.runAndWait()
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
+
+    audio = r.recognize_google(audio)
+
+    if audio.lower() == "notepad":
+        engine.say("Starting Notepad")
+        engine.runAndWait()
+        os.system("start notepad")
+        engine.say("PLEASE TELL ME WHAT YOU WANT TO WRITE IN NOTEPAD")
+        engine.runAndWait()
+        with mic as source:
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            audio = r.listen(source)
+
+        audio = r.recognize_google(audio)
+        f = file.write(audio)
+        continue
+
+    elif audio.lower()=="exit":
+        engine.say("EXITING ! THANK YOU FOR CHOOSING ME, HAVE A NICE DAY !")
+        engine.runAndWait()
+        exit()
+    else:
+        engine.say("I CAN'T RECOGNIZE YOU SIR ! PLEASE TRY AGAIN.")
+        engine.runAndWait()
+
+file.close()
+
+
+
